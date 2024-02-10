1c4d239f065711b9e9166dd5376b05ba97e56e8d
Panda1337 <78670879+PdA1337@users.noreply.github.com>
2024-02-10 11:03:36 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 /*
 爱企查vip
-^https:\//aiqicha.baidu.com\/usercenter\/getvipinfoajax url script-response-body https://raw.githubusercontent.com/PdA1337/WebFile/main/test.js
-mitm=aiqicha.baidu.com
+^https:\//api-hdcjgo.9w9.com\/login url script-response-body https://raw.githubusercontent.com/PdA1337/WebFile/main/test.js
+mitm=api-hdcjgo.9w9.com
 */
 
 var body = $response.body
-    .replace(/\"svip\":\{\"status\":0,\"startTime\":\"2023-01-12\",\"endTime\":\"2023-01-18\"/, "\"svip\":\{\"status\":1,\"startTime\":\"\",\"endTime\":\"2099-12-31\"");
+    .replace(/\"user_type\":0/, "\"user_type\":1");
 $done({ body });
