e3249c1ce9e0ced26c781ffbdd2ed72a02491d4e
Panda1337 <78670879+PdA1337@users.noreply.github.com>
2024-02-10 12:18:09 +0800

    Update test.js

--- test.js
+++ test.js
@@ -5,5 +5,5 @@ mitm=api-hdcjgo.9w9.com
 */
 
 var body = $response.body
-    .replace(/\"user_type\":0/, "\"user_type\":0").replace("\"vipEndTime\":0","\"vipEndTime\":9999999999").replace("\"brandVipType\":0","\"brandVipType\":0").replace("\"isManager\":0","\"isManager\":1");
+    .replace(/\"user_type\":0/, "\"user_type\":2").replace("\"vipEndTime\":0","\"vipEndTime\":9999999999").replace("\"brandVipType\":0","\"brandVipType\":1").replace("\"isManager\":0","\"isManager\":1");
 $done({ body });
